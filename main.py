4fb90aaf1329b967aef7eea09817157f4fcb2910
Abhishek Kumar <abhishek.kumar@ennoventure.com>
2022-11-03 11:20:56 +0530

    completed FastAPI app and Dockerfile part.

---
+++ main.py
@@ -0,0 +1,24 @@
+from typing import Union
+
+from fastapi import FastAPI
+import uvicorn
+
+from source.roman_number_converter import RomanNumeralsConverter
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/roman_converter/{integer_number}")
+def read_item(integer_number: int, Converted_Roman_Numberal: Union[str, None] = None):
+    roman_converter_obj = RomanNumeralsConverter()
+    roman_letter = roman_converter_obj.roman_converter(integer_number)
+    return {"integer_number": integer_number, "converted_roman_numberal": roman_letter}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app)
